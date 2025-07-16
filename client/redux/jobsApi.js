8b93f59fa8371326afc65edf45f7b97be9c05963
shaikh faiz <faizuddinshaikh55@gmail.com>
2025-07-17 00:28:24 +0530

    react table uninstalled

--- client/redux/jobsApi.js
+++ client/redux/jobsApi.js
@@ -2,7 +2,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: `http://localhost:5000/api/jobs`,
+    baseUrl: `https://khizar-server.vercel.app/api/jobs`,
     
 
 });
